fb2b3c266e777963d7b8364a56e2f381a5eb255e
ENERGEABC <energeabc1@gmail.com>
2026-08-13 19:23:53 +0200

    Create TideModels.kt

---
+++ app/src/main/java/com/samil/marin/TideModels.kt
@@ -0,0 +1,24 @@
+package com.samil.marin
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+data class TideEvent(
+    val dateTime: LocalDateTime,
+    val heightMeters: Double,
+    val isHighTide: Boolean
+) {
+    companion object {
+        private val FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+
+        fun parseDateTime(raw: String): LocalDateTime = LocalDateTime.parse(raw, FORMATTER)
+    }
+}
+
+data class TideState(
+    val previousEvent: TideEvent,
+    val nextEvent: TideEvent,
+    val progressFraction: Double,
+    val rising: Boolean
+)
+
